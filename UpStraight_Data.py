100f6b837eb93a5b0e0155270c003d5908785453
Lasse Meixner <lassemeixner@gmail.com>
2023-04-23 17:51:36 +0100

    docstrings and joiner functions

--- UpStraight_Data.py
+++ UpStraight_Data.py
@@ -1,18 +1,50 @@
+import os
+
 from datetime import datetime, time
 import pandas as pd
 from tqdm import tqdm
+from apple_watch_data_package.Apple_Data import light_preprocess, fill_stand_time
 
 state_mapping = {-1:"Unavailable",0:"Lying",1:"Sitting",2:"Standing",3:"Active"}
 posture_mapping = {-1:"Unavailable",0:"Straight",1:"Slouched"}
 
+feature_columns = ["AppleStandTime","ActiveEnergyBurned","BasalEnergyBurned","HeartRate","DistanceWalkingRunning","StepCount"]
+
 
 def preprocess(data_export):
+    """Auxiliary function to preprocess the appData.
+
+    Args:
+        data_export (df): df
+
+    Returns:
+        df: df
+    """
     data_export["date"] = pd.to_datetime(data_export["date"])
+    data_export["day_date"] = data_export["date"].dt.date
     data_export["day"] = data_export["date"].dt.day
     data_export["time"] = data_export["date"].dt.time
     data_export["state_string"] = data_export["state"].map(state_mapping)
     data_export["posture_string"] = data_export["posture"].map(posture_mapping)
     return data_export
+
+def get_appData(path = "../data/", save = False):
+    df = pd.concat([pd.read_csv(f"{path}{f}").assign(source = f.split(".")[0].split("_")[1]) for f in os.listdir("../data") if f.startswith("export_")])
+    # can preprocess concatenated file since no step depends on individual data
+    df = preprocess(df)
+    if save:
+        df.to_csv("../data/appData.csv",index=False)
+    return df
+
+def get_health_data(path = "../data/", save = False):
+    # load and light preprocess each health data file separately
+    health = pd.concat([
+        fill_stand_time(
+        light_preprocess(
+            pd.read_csv(f"{path}{f}").assign(source = f.split(".")[0].split("_")[2]))) for f in os.listdir("../data") if f.startswith("health_filtered_")])
+    if save:
+        health.to_csv("../data/health.csv",index=False)
+    return health
 
 def build_features(appData,health):
     # drop unavailable entries
@@ -39,6 +71,16 @@ def build_features(appData,health):
     return appData
 
 def build_simple_interval_features(appData,health_subset,i,interval,column):
+    """Auxiliary function
+    For each entry in the appData, get the mean, max, min, std, and range of the 'column' type of health data for the interval before the notification.
+
+    Args:
+        appData (df): appData
+        health_subset (df): health_df for the interval before the notification
+        i (index): index of corresponding notification entry in appData
+        interval (str): time interval
+        column (str): health type
+    """
     # get the right column
     base = health_subset[health_subset["type"]==column]
     appData.loc[i,f"{column}_{interval}_mean"] = base["value"].mean()
