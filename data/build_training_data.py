ce831ec19e59b74a86f3a244fdbeaa5af1be91fa
Lasse Meixner <lassemeixner@gmail.com>
2023-06-21 22:56:41 +0200

    add wrapper to build training data

---
+++ data/build_training_data.py
@@ -0,0 +1,8 @@
+# Wrapper script to automate training file building process
+import sys
+sys.path.append("../")
+
+from UpStraight_Train import build_training_data
+
+if __name__ == "__main__":
+    build_training_data(path = "../data/", save = True)
