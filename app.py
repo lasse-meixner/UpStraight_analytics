97f40693fa35042845a70f8ff6f4ca81b23086a8
Lasse Meixner <lassemeixner@gmail.com>
2023-06-23 16:43:04 +0200

    include prediction graph

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Dash app for data visualization
+import os
 
 import dash
 from dash.dependencies import Input, Output
@@ -7,13 +8,21 @@ import plotly.express as px
 import pandas as pd
 
 from UpStraight_Data import feature_columns
-from UpStraight_Visualize import plot_day_plotly
+from UpStraight_Visualize import plot_day_plotly, plot_day_prediction_plotly
 
 
-# load data
-appData = pd.read_csv("data/appData.csv")
-health = pd.read_csv("data/health.csv")
+
+
+# load available prediction files in data folder as dict, with user as key, and df as value, if it exists
+pred_files = {f.split(".")[0].split("_")[1]:pd.read_csv(f"data/{f}", converters = {"date": pd.to_datetime}) for f in os.listdir("data/") if f.startswith("pred_")}
+
+# load data. 
+appData = pd.read_csv("data/appData.csv", converters= {"date": pd.to_datetime, "day_date": pd.to_datetime})
+health = pd.read_csv("data/health.csv", converters= {"date": pd.to_datetime, "day_date": pd.to_datetime})
 health = health[health["type"].isin(feature_columns)]
+
+def get_day(source, entry):
+    return appData.query("source==@source").groupby("day_date").count().sort_values("date",ascending=False).index[entry-1]
 
 # build app
 app = dash.Dash(__name__)
@@ -42,31 +51,39 @@ app.layout = html.Div([
         html.Div([
             html.Label("Day"),
             dcc.Dropdown(
-                id="day",
+                id="day_entry",
                 value=1,
             ),
 
         ],style={"width": "48%", "display": "inline-block"}),
     ]),
     dcc.Graph(id="graph"),
+    dcc.Graph(id="graph_pred")
 ])
 
 @app.callback(
     Output("graph", "figure"),
     [Input("column", "value"),
     Input("source", "value"),
-    Input("day", "value")])
-def update_graph(column, source, day):
-    fig = plot_day_plotly(appData.query("source==@source"),health.query("source==@source"),column,day)
+    Input("day_entry", "value")])
+def update_graph(column, source, day_entry):
+    fig = plot_day_plotly(appData.query("source==@source"),health.query("source==@source"),column,get_day(source,day_entry))
     return fig
 
 # callback for day dropdown depending on selected source
 @app.callback(
-    Output("day", "options"),
+    Output("day_entry", "options"),
     [Input("source", "value")])
 def update_day_options(source):
     return [{"label": i, "value": i} for i in range(1,appData.query("source==@source").groupby("day_date").count().shape[0]+1)]
 
+@app.callback(
+    Output("graph_pred", "figure"),
+    [Input("source", "value"),
+     Input("day_entry", "value")])
+def update_graph_pred(source, day_entry):
+    fig = plot_day_prediction_plotly(pred_files[source], get_day(source,day_entry))
+    return fig
 
 if __name__ == "__main__":
     app.run_server(debug=True, port=8050)
