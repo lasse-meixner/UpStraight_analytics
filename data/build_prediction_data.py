9841d02c1a88ad48c1676794c3387b6a9c7084dd
Lasse Meixner <lassemeixner@gmail.com>
2023-06-30 12:34:46 +0200

    add training data back (and test sampling)

--- data/build_prediction_data.py
+++ data/build_prediction_data.py
@@ -10,20 +10,34 @@ from UpStraight_Train import training_columns
 
 @click.command()
 @click.option("--user", default="cr", help="User to build prediction data for")
-def build_pred_data(user, save = True):
+@click.option("--sample", default=0.1, help="Option to test function on sample of data")
+def build_pred_data(user, sample, save = True):
     health_p = process_user_health(pd.read_csv(f"../data/health_filtered_{user}.csv"))
+
+    if sample:
+        health_p = health_p.sample(frac=sample, random_state=42)
+
     # get a predictions dataframe 
     start = health_p["start"].min()
     end = health_p["end"].max()
 
     pred = pd.DataFrame(pd.date_range(start=start, end=end, freq="15min"), columns=["date"])
-    date_col = pred["date"]
 
     # add hour column manually
     pred["hour"] = pred["date"].dt.hour
 
     # add features
     pred = build_features(pred, health_p)
+
+    # add training data from source
+    train_source = pd.read_csv("../data/train.csv", converters={"date": pd.to_datetime}).query("source==@user")
+    train_source["train"] = True
+    train = train_source[training_columns + ["date", "train", "state", "state_string", "posture", "posture_string"]]
+    pred = pd.concat([pred, train], axis=0).reset_index(drop=True)
+
+    # copy date & train columns to add back after prediction, then prepare for prediction
+    date_col = pred["date"]
+    train_cols = pred[["train", "state", "state_string", "posture", "posture_string"]]
     pred = pred[training_columns].dropna()
 
     # load user model
@@ -34,7 +48,11 @@ def build_pred_data(user, save = True):
     pred["proba"] = model.predict_proba(pred)[:,1]
 
     # add back date column
-    pred["date"] = date_col
+    pred["date"] = date_col.astype("datetime64[ns]")
+    # add back train columns
+    pred[["train", "state", "state_string", "posture", "posture_string"]] = train_cols
+    # fill NA for train
+    pred["train"] = pred["train"].fillna(False)
     
     if save:
         pred.to_csv(f"../data/pred_{user}.csv", index=False)
